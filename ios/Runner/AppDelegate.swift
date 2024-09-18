dc3fdb4d10f3ae015cc175bda6e6f5214636bfec
ber <bernardomelofacu@gmail.com>
2024-09-18 19:37:54 -0300

    IOS CHANNEL

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,21 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
+    let controller : FlutterViewController  = window?.rootViewController as! FlutterViewController
+    let nativoChannel = FlutterMethodChannel(name: "meuapp.com.br/nativo", binaryMessenger: controller.binaryMessenger)
+     nativoChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> void in guard call.method == "calcSum" 
+      else {result(FlutterMethodNotImplemented);return}
+      if let args = call.arguments as? [String: Any],
+      let a = arguments["a"] as? Int, 
+       let a = arguments["a"] as? Int{
+        result(a+b)
+       }else{result(-1)}
+      
+      })
+
+
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
