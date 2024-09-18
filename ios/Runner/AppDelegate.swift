c01bc9383d8fa3228cd10caa07e9870634918eea
ber <bernardomelofacu@gmail.com>
2024-09-18 19:39:48 -0300

    Correcao

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,20 +7,23 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller : FlutterViewController  = window?.rootViewController as! FlutterViewController
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
     let nativoChannel = FlutterMethodChannel(name: "meuapp.com.br/nativo", binaryMessenger: controller.binaryMessenger)
-     nativoChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> void in guard call.method == "calcSum" 
-      else {result(FlutterMethodNotImplemented);return}
-      if let args = call.arguments as? [String: Any],
-      let a = arguments["a"] as? Int, 
-       let a = arguments["a"] as? Int{
-        result(a+b)
-       }else{result(-1)}
+    
+    nativoChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      guard call.method == "calcSum" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
       
-      })
-
-
+      if let args = call.arguments as? [String: Any],
+         let a = args["a"] as? Int,
+         let b = args["b"] as? Int {
+        result(a + b) // Retorna a soma de `a` e `b`
+      } else {
+        result(FlutterError(code: "INVALID_ARGUMENT", message: "Argumentos inválidos", details: nil)) // Retorna um erro se os argumentos forem inválidos
+      }
+    }
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
